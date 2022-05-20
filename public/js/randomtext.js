99ffd418e61b2b73a64f69b1fbbe9d448770384a
$~Bunni <emily@bunni.me>
2022-05-20 13:09:20 -0400

    Typing Animation

--- public/js/randomtext.js
+++ public/js/randomtext.js
@@ -1,9 +1,21 @@
+// function randomtext() {
+//     var randomtxt = [
+//         'Practice',
+//         'AI Practice',
+//         'Smooth Knockback'];
+//     return randomtxt[Math.floor((Math.random() * 5.99))];
+// }
+
+
+// document.getElementById("spam").innerHTML = randomtext();
+
 function randomtext() {
     var randomtxt = [
         'Practice',
         'AI Practice',
         'Smooth Knockback'];
-    return randomtxt[Math.floor((Math.random() * 5.99))];
+    return randomtxt[Math.floor((Math.random() * 3.99))];
 }
-
 document.getElementById("spam").innerHTML = randomtext();
+
+
--- src/jsx/Core.js
+++ src/jsx/Core.js
@@ -2,14 +2,16 @@ import "./css/Core.css";
 import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import GenericFasIcon from "./componetry/GenericFasIcon";
 import { faServer, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { Tooltip } from "@mui/material";
+import TypeAnimation from "react-type-animation";
 
 function App() {
   return (
     
     <div className="h-screen flex justify-center items-center flex-col gap-y-6 flex-wrap dark:bg-gray-900 bg-white">
+    <img alt="Logo" src="/logo.png" width="256px" />
+
 		<div className="intro">Esta Network</div>
-		<div className="tagline"><p id="spam"></p></div>
+    <TypeAnimation className="Core-Typewrite" cursor={true} bold={true} sequence={["Practice & AI Practice"]} wrapper="h1" />
     <div className="row">
     <GenericFasIcon icon={faDiscord} size="2x" tooltip="Click me to join our Discord server" />
     <GenericFasIcon icon={faShoppingCart} size="2x" tooltip="Click me to visit our store" />
